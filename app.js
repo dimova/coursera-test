aad9b452e0ff7c128ce7e2c0a344e0673f62b887
Desi Dimova <dimova@gmail.com>
2023-04-16 17:27:53 +0300

    files

--- app.js
+++
@@ -1,9 +0,0 @@
-(function (){
-    'use strict';
-    angular.module('MyFirstApp', [])
-    .controller('MyFirstController', function($scope){
-        $scope.name = "Yakov";
-        $scope.sayHello = function () { return "Hello Coursera!";
-     }
-    });
-})();
